c9d737da31f3940a07b8a9aee36f74ab7b419e70
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2016-10-22 14:10:16 +0200

    adds an exercise for question 993

---
+++ src/main/java/enthuware/ocajp/i/v8_2/Q_993.java
@@ -0,0 +1,22 @@
+package enthuware.ocajp.i.v8_2;
+
+/**
+ * @author daniel.bubenheim@gmail.com
+ */
+public class Q_993 {
+
+    public static void main(final String args[]) {
+        A a = new A();
+        B b = new B();
+        a = b;
+        b = a; // Will not compile because 'a' is declared of class A and 'b' is of B which is a subclass of A. So an explicit cast is needed.
+        a = (B) b;
+        b = (B) a;
+    }
+}
+
+class A {
+}
+
+class B extends A {
+}

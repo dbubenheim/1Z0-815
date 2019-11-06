309bc4b97f1ace3b84fdcf216e722fb926c9d1c4
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2019-11-06 16:57:49 +0100

    aktualisierung der pom und eineuebung zu stringbuilder

---
+++ src/main/java/com/udemy/Chapter5UnderstandingStringsQuestion9.java
@@ -0,0 +1,14 @@
+package com.udemy;
+
+public class Chapter5UnderstandingStringsQuestion9 {
+
+    public static void main(String[] args) {
+
+        int total = 0;
+        StringBuilder letters = new StringBuilder();
+        total += letters.substring(1, 2).length();
+        total += letters.substring(6, 6).length();
+        total += letters.substring(6, 5).length(); // Exception in thread "main" java.lang.StringIndexOutOfBoundsException: start 1, end 2, length 0
+        System.out.println(total);
+    }
+}
